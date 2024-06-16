20051c1f8b480903389db2b7ebfa897d85ace2da
morit <moritz.ullrich@googlemail.com>
2024-06-16 19:00:31 +0200

    teste stuff

--- CARD-GAME/src/main/java/de/thowl/CARD_GAME/Cards/Card.java
+++ CARD-GAME/src/main/java/de/thowl/CARD_GAME/Cards/Card.java
@@ -5,14 +5,21 @@ import de.thowl.CARD_GAME.Cards.CardValue;
 @Data
 
 
-public class Card {
+public class Card : IEquatable<Card>
+{
 
     private String CardId;
     private CardType Type;
     private CardValue Value;
     private String Card;
 
-    
+    public Karte(CardType cType, CardValue cValue)
+    {
+        Type = cType;
+        Value = cValue;
+        
+
+    }
     public string CreateCards() {
         for (int i = 0; i < 8; i++) {
             
